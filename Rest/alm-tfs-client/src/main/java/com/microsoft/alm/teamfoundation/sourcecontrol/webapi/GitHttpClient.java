f4678960f15e0ea7bf8029f5d2ea608a0ad5309f
Alex Rukhlin <arukhlin@microsoft.com>
2016-12-21 09:39:11 -0500

    Add work iyem request to Create PR method

--- Rest/alm-tfs-client/src/main/java/com/microsoft/alm/teamfoundation/sourcecontrol/webapi/GitHttpClient.java
+++ Rest/alm-tfs-client/src/main/java/com/microsoft/alm/teamfoundation/sourcecontrol/webapi/GitHttpClient.java
@@ -5,11 +5,18 @@ package com.microsoft.alm.teamfoundation.sourcecontrol.webapi;
 
 import java.io.InputStream;
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import com.microsoft.alm.client.HttpMethod;
+import com.microsoft.alm.client.model.NameValueCollection;
+import com.microsoft.alm.client.VssMediaTypes;
+import com.microsoft.alm.client.VssRestRequest;
 
 import com.microsoft.alm.client.VssRestClientHandler;
 import com.microsoft.alm.client.utils.ArgumentUtility;
+import com.microsoft.alm.visualstudio.services.webapi.ApiResourceVersion;
 
 public class GitHttpClient extends GitHttpClientBase {
 
@@ -434,5 +441,42 @@ public class GitHttpClient extends GitHttpClientBase {
     public List<GitRepository> getRepositories(final String project, final boolean includeLinks) {
         ArgumentUtility.checkStringForNullOrEmpty(project, "project", true); //$NON-NLS-1$
         return super.getRepositories(project, includeLinks);
+    }
+
+    /** 
+     * [Preview API 3.1-preview.1] Create a git pull request
+     * 
+     * @param gitPullRequestToCreate 
+     *            
+     * @param repositoryId 
+     *            
+     * @param linkBranchWorkitems 
+     *            
+     * @return GitPullRequest
+     */
+    public GitPullRequest createPullRequest(
+        final GitPullRequest gitPullRequestToCreate, 
+        final String repositoryId, 
+        final Boolean linkBranchWorkitems) { 
+
+        final UUID locationId = UUID.fromString("9946fd70-0d40-406e-b686-b4744cbbcc37"); //$NON-NLS-1$
+        final ApiResourceVersion apiVersion = new ApiResourceVersion("3.1-preview.1"); //$NON-NLS-1$
+
+        final Map<String, Object> routeValues = new HashMap<String, Object>();
+        routeValues.put("repositoryId", repositoryId); //$NON-NLS-1$
+
+        final NameValueCollection queryParameters = new NameValueCollection();
+        queryParameters.addIfNotNull("linkBranchWorkitems", linkBranchWorkitems); //$NON-NLS-1$
+
+        final VssRestRequest httpRequest = super.createRequest(HttpMethod.POST,
+                                                               locationId,
+                                                               routeValues,
+                                                               apiVersion,
+                                                               gitPullRequestToCreate,
+                                                               VssMediaTypes.APPLICATION_JSON_TYPE,
+                                                               queryParameters,
+                                                               VssMediaTypes.APPLICATION_JSON_TYPE);
+
+        return super.sendRequest(httpRequest, GitPullRequest.class);
     }
 }
